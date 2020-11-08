b7d9130c254bf556700ea475b8099cbafb7d5568
Abdelilah Elhaddad <elhaddadabdelilah@gmail.com>
2020-11-08 17:11:37 +0100

    First commit faq-accordion-card-main

---
+++ main.js
@@ -0,0 +1,10 @@
+
+let accordion = document.getElementsByClassName("accordion");
+
+
+
+for(i= 0; i<accordion.length; i++){
+  accordion[i].addEventListener('click',function () {
+    this.classList.toggle('active');
+  })
+}
